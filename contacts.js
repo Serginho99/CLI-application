50b3c2f131d15f6c6adba6980eb6286e49281e2f
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-11-26 22:12:22 +0200

    create-hw

---
+++ contacts.js
@@ -0,0 +1,65 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function getContacts() {
+  const contacts = await fs.readFile(contactsPath);
+  const parsedContacts = JSON.parse(contacts);
+  return parsedContacts;
+}
+
+async function getContactById(contactId) {
+  const strContactId = String(contactId);
+  const contacts = await getContacts();
+  const contactById = await contacts.find(
+    (contact) => contact.id === strContactId
+  );
+  if (!contactById) {
+    return null;
+  }
+  return contactById;
+}
+
+async function removeContact(contactId) {
+  const strContactId = String(contactId);
+  const contacts = await getContacts();
+  const idx = contacts.findIndex((contact) => contact.id === strContactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [deleteContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return deleteContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await getContacts();
+  const newContact = { id: nanoid(), name, email, phone: String(phone) };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+async function updateContactById(id, name, email, phone) {
+  const strContactId = String(id);
+  const contacts = await getContacts();
+  const idxContact = contacts.findIndex(
+    (contact) => contact.id === strContactId
+  );
+  if (idxContact === -1) {
+    return null;
+  }
+  contacts[idxContact] = { id, name, email, phone };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return contacts[idxContact];
+}
+
+module.exports = {
+  getContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+};
